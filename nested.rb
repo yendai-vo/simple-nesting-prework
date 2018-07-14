37e6fb3518114d99ff61aaaa0655d8f92c2781a1
Cindy Vo <yendai.vo@hotmail.com>
2018-07-14 17:05:59 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -1,19 +1,9 @@
 
 def hopper
 	programmer_hash =
- 		{
-        :grace_hopper => {
-          :known_for => "COBOL",
+ 		{:known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
-        }
+        
      }
 
 
